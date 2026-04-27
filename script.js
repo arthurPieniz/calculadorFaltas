4b16f0db7875a7026a88f6381ecd533564441756
arthurPieniz <102185959+arthurPieniz@users.noreply.github.com>
2026-04-26 23:15:54 -0300

    update

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
 function calcule(){
 
-var pctg = document.getElementById("porcentagem").value/100;
-var nPre = document.getElementById("nPresenca").value;
-var nTot = document.getElementById("nTotal").value;
-var nFal = document.getElementById("nFaltas").value;
-var pDia = document.getElementById("periodosDia").value;
+let pctg = document.getElementById("porcentagem").value/100;
+let nPre = document.getElementById("nPresenca").value;
+let nTot = document.getElementById("nTotal").value;
+let nFal = document.getElementById("nFaltas").value;
+let pDia = document.getElementById("periodosDia").value;
 
 console.log(pDia);
 
-var x = (nPre - pctg*nTot - pctg*pDia*nFal)/(pctg*pDia - pDia)
+let x = (nPre - pctg*nTot - pctg*pDia*nFal)/(pctg*pDia - pDia);
 
-x = Math.ceil(x)
+x = Math.ceil(x);
 
-document.getElementById("result").innerHTML = "Você precisa vir: " + x
+document.getElementById("result").innerHTML = "Você precisa vir: " + x;
+
+}
+
+function calcule2(){
+    let pctg = document.getElementById("porcentagem2").value/100;
+    let nPre = document.getElementById("nPresenca2").value;
+    let nTot = document.getElementById("nTotal2").value;
+    let pDia = document.getElementById("periodosDia2").value;
+
+    console.log(pDia);
+
+    let y = (nPre - pctg*nTot)/(pctg*pDia);
+    console.log(y);
+    y = Math.floor(y);
+
+    document.getElementById("result2").innerHTML = "Você pode faltar: " + y;
 
 }
